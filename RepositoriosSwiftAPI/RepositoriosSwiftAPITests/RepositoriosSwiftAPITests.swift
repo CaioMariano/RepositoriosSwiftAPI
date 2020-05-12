c6fd86fec6110e59e7665fe283898852485a07af
Caio Araujo Mariano <caio.araujo.mariano@hotmail.com>
2020-05-12 20:13:31 -0300

    Initial commit

---
+++ RepositoriosSwiftAPI/RepositoriosSwiftAPITests/RepositoriosSwiftAPITests.swift
@@ -0,0 +1,34 @@
+//
+//  RepositoriosSwiftAPITests.swift
+//  RepositoriosSwiftAPITests
+//
+//  Created by Caio Araujo Mariano on 12/05/20.
+//  Copyright © 2020 Caio Araujo Mariano. All rights reserved.
+//
+
+import XCTest
+@testable import RepositoriosSwiftAPI
+
+class RepositoriosSwiftAPITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
