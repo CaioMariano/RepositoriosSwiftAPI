427390f6a285ee03b25bacb6e80598a573109df2
Caio Araujo Mariano <caio.araujo.mariano@hotmail.com>
2020-05-13 18:08:54 -0300

    feat(repositories_view): create layout and organize pastes

---
+++ RepositoriosSwiftAPI/RepositoriosSwiftAPI/RepositoriesList/ViewModel/RepositoriesViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  RepositoriesViewModel.swift
+//  RepositoriosSwiftAPI
+//
+//  Created by Caio Araujo Mariano on 13/05/20.
+//  Copyright © 2020 Caio Araujo Mariano. All rights reserved.
+//
+
+import UIKit
+
+protocol RepositoriesViewModelProtocol {}
+
+class RepositoriesViewModel: RepositoriesViewModelProtocol {
+    
+}
